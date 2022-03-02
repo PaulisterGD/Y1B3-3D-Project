27ee8cbde1601aa7ca9dc7168aaa135aefdcd95d
PaulisterGD <paullambropoulos93@gmail.com>
2022-03-02 10:17:13 +0100

    First proper commit of project files?
    Gee idk

---
+++ Assets/flyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Fly script added to: " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
